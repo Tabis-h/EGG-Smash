1f38a5cb9e0cc25e030f974d95764821c99020ce
Tabis-h <tabishsubedar50@gmail.com>
2024-06-28 00:17:39 +0530

    more movement updating

--- EGG Smash/Assets/PlayerMovment.cs
+++ EGG Smash/Assets/PlayerMovment.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
@@ -6,35 +5,61 @@ public class PlayerMovment : MonoBehaviour
     public Rigidbody rb;
     public float forwardforce = 2000f;
     public float sidewaysforce = 500f;
-   
-   
-    // Update is called once per frame
+    public float jumpforce = 500f; 
+    private bool isGrounded;
+    
+
     void Update()
     {
-        
-
-        if(Input.GetKey("a"))
+        // Move left
+        if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysforce*Time.deltaTime,0,0);
-        }
-         if(Input.GetKey("d"))
-        {
-            rb.AddForce(sidewaysforce*Time.deltaTime,0,0);
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-
-         if(Input.GetKey("w"))
+        // Move right
+        if (Input.GetKey("d"))
         {
-            rb.AddForce(0,0,forwardforce * Time.deltaTime);
+            rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-         if(Input.GetKey("s"))
+        // Move forward
+        if (Input.GetKey("w"))
         {
-            rb.AddForce(0,0,-forwardforce * Time.deltaTime);
+            rb.AddForce(0, 0, forwardforce * Time.deltaTime, ForceMode.VelocityChange);
         }
 
+        // Move backward
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardforce * Time.deltaTime, ForceMode.VelocityChange);
+        }
 
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(0, jumpforce, 0, ForceMode.Impulse);
+            isGrounded = false; // Set isGrounded to false to prevent double jumping
+        }
+    }
 
+    // Detect collision with ground
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is touching the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; 
+        }
+    }
 
+    // Detect when the player is no longer colliding with the ground
+    void OnCollisionExit(Collision collision)
+    {
+        // Check if the player is no longer touching the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; 
+        }
     }
 }
