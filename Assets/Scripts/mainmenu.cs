5d3d1a53fe483898df8a3e08e64b345678845925
Tabis-h <tabishsubedar50@gmail.com>
2024-07-01 23:18:54 +0530

    main menu added

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    
+        public void play()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+
+        public void quit()
+        {
+            Application.Quit();
+            Debug.Log("game quit");
+        }
+    
+}
